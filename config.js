ee7dbd91f2c31f4298eb452c04cf29b1a61b8a78
jllance <66705072+jllance@users.noreply.github.com>
2020-10-03 10:24:34 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "396962758370566",
+    clientSecret: "5bbcd784e84340fbe65464cf4aae4680",
+  },
+};
